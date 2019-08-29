4e6d612049c7008bd765d7396d38962808b2687c
Matteo Lunghi <39874742+mlunghi@users.noreply.github.com>
2019-08-29 15:37:01 +0400

    Delete ICustomer.java

--- ICustomer.java
+++
@@ -1,9 +0,0 @@
-package login.submit.register;
-
-public interface ICustomer {
-
-    public int insertCustomer(Customer customer); //Method to insert a customer into the database
-    public Customer getCustomer(String username, String password); //Method to fetch a customer from database
-
-
-}
